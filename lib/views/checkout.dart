111a0a78d15b1b8226ba4686cfb8ff659cb9bd1b
dhirajpakhrin007 <dhirajpakhrin95@gmail.com>
2021-04-20 15:20:50 +0545

    myfirst

--- lib/views/checkout.dart
+++ lib/views/checkout.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CheckOut extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
